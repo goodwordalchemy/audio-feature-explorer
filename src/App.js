604ae2b75c186e1bd7606b387ec58f6b22457dcb
David Goldberg <goodwordachemy@gmail.com>
2018-11-29 10:06:19 -0500

    formatted track cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,52 @@ import React, {
 } from 'react';
 import './App.css';
 
+const supportedAudioFeatures = [
+    "danceability", "energy", "key", "loudness", "mode", "speechiness",
+    "acousticness", "instrumentalness", "liveness", "valence",
+    "tempo", "time_signature"
+];
+
+const filterObjectKeys = function(obj, keysToKeep){
+    const filtered = Object.keys(obj)
+        .filter(key => keysToKeep.includes(key))
+        .reduce((acc, key) => {
+            return {
+                ...acc, 
+                [key]: obj[key]
+            };
+        }, {});
+
+    return filtered
+}
+
+class TrackCard extends Component {
+
+    render(){
+        const trackInfo = this.props.trackInfo;
+        const audioFeaturesString = JSON.stringify(
+            filterObjectKeys(trackInfo[1], supportedAudioFeatures),
+            null, 2);
+
+        const relaventTrackInfo = {
+            trackName: trackInfo[0]['name'],
+            albumName: trackInfo[0]['album']['name'],
+            artistNames: trackInfo[0]['artists'].map((a) => a['name']).join(", "),
+        }
+
+        return (
+            <div className="track-card">
+                <div>trackName: {relaventTrackInfo.trackName}</div>
+                <div>albumName: {relaventTrackInfo.albumName}</div>
+                <div>artistNames: {relaventTrackInfo.artistNames}</div>
+                <div>audioFeatures: <pre><code>{audioFeaturesString}</code></pre></div>
+                
+            </div>
+        )
+    }
+}
+
+
 class App extends Component {
     state = {
         response: [],
@@ -44,10 +90,9 @@ class App extends Component {
 
     render() {
         const tracks = this.state.response.map((track) =>{
-            const trackString = JSON.stringify(track);
 
             return (
-                <li className="track-card">{trackString}</li>
+                <li className="track-card-container"><TrackCard trackInfo={track} /></li>
             )
         });
 
