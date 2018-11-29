98f58d78259871363ce699a8f32056e629935401
David Goldberg <goodwordachemy@gmail.com>
2018-11-29 08:34:24 -0500

    serving test data

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+'use strict';
+const fs  = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
 const port = process.env.PORT || 5000;
+const sampleRecommendationsFile = 'tracks-with-features.json'
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get('/api/hello', (req, res) => {
+const getSampleRecommendations = () => {
+	let sampleRecommendations = fs.readFileSync(sampleRecommendationsFile);
+	sampleRecommendations = JSON.parse(sampleRecommendations);
+	
+	return sampleRecommendations
+}
+
+app.get('/api/recommendations', (req, res) => {
     res.send({
-        express: 'Hello From Express'
+        recommendations: getSampleRecommendations()
     });
 });
 app.post('/api/world', (req, res) => {
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {
     Component
 } from 'react';
 import './App.css';
+
 class App extends Component {
     state = {
         response: '',
@@ -12,13 +13,13 @@ class App extends Component {
     componentDidMount() {
         this.callApi()
             .then(res => this.setState({
-                response: res.express
+                response: JSON.stringify(res.recommendations)
             }))
             .catch(err => console.log(err));
     }
 
     callApi = async () => {
-        const response = await fetch('/api/hello');
+        const response = await fetch('/api/recommendations');
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
         return body;
